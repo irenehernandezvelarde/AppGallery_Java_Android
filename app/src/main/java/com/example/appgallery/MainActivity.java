22403f740c0cf2a3ee79321bde885994ccc70734
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2023-01-09 16:40:01 +0100

    //Implementamos la version 2. //Creamos nuevo boton para la camara.  //Creamos un nuevo intent, activityResultLauncher y nueva funcion openSomeActivityForResult2.

--- app/src/main/java/com/example/appgallery/MainActivity.java
+++ app/src/main/java/com/example/appgallery/MainActivity.java
@@ -8,27 +8,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public ActivityResultLauncher<Intent> someActivityResultLauncher = null;
+    public ActivityResultLauncher<Intent> someActivityResultLauncher2 = null;
     public static int RC_PHOTO_PICKER = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button abrirGale = findViewById(R.id.abrirGaleria);
+        Button abrirCamara = findViewById(R.id.camaraButton);
+
+
         this.someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
-                            // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
                             ImageView imageView = findViewById(R.id.img);
@@ -42,12 +48,39 @@ public class MainActivity extends AppCompatActivity {
                 openSomeActivityForResult(null);
             }
         });
+
+
+        this.someActivityResultLauncher2 = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Bundle extras = result.getData().getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ImageView imagen = findViewById(R.id.img);
+                            imagen.setImageBitmap(imageBitmap);
+                        }
+                    }
+                });
+        abrirCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSomeActivityForResult2(null);
+            }
+        });
+
     }
     public void openSomeActivityForResult(View view) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         someActivityResultLauncher.launch(intent);
+    }
+
+    public void openSomeActivityForResult2(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        someActivityResultLauncher2.launch(intent);
     }
 
 }
