316c9965fdf45f1bc44ec6c9a3a309a311cdef09
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2023-03-27 16:31:40 +0200

    //commit

--- app/src/main/java/com/example/appgallery/MainActivity.java
+++ app/src/main/java/com/example/appgallery/MainActivity.java
@@ -5,21 +5,29 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     public ActivityResultLauncher<Intent> someActivityResultLauncher = null;
     public ActivityResultLauncher<Intent> someActivityResultLauncher2 = null;
     public static int RC_PHOTO_PICKER = 0;
+    public Uri uriPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +64,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == RESULT_OK) {
-                            Bundle extras = result.getData().getExtras();
-                            Bitmap imageBitmap = (Bitmap) extras.get("data");
                             ImageView imagen = findViewById(R.id.img);
-                            imagen.setImageBitmap(imageBitmap);
+                            imagen.setImageURI(uriPhoto);
                         }
                     }
                 });
@@ -79,8 +85,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openSomeActivityForResult2(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        someActivityResultLauncher2.launch(intent);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            if (photoFile != null) {
+                uriPhoto = FileProvider.getUriForFile(this, "com.example.appgallery.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriPhoto);
+            }
+        someActivityResultLauncher2.launch(takePictureIntent);
+    }
+
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
